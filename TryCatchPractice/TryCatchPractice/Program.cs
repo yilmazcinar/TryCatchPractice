cab9e1d67648ee2d944d8d80ffc6b5c8be8cf8e5
yilmazcinar <cinaryilmaz96@gmail.com>
2025-01-19 19:59:40 +0300

    Add files via upload

---
+++ TryCatchPractice/TryCatchPractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        
+        while (true) // Sonsuz döngü, kullanıcı geçerli bir sayı girene kadar devam edecek.
+        {
+            
+
+            try
+            {
+                Console.Write("\nLütfen bir sayı giriniz:\n ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"\nGirdiğiniz sayını karesi : {Math.Pow(sayi ,2)}\n");
+
+                break;
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("\nGeçersiz giriş yaptınız lütfen bir sayı giriniz. \n");
+            }
+
+
+
+          
+        }
+    }
+}
